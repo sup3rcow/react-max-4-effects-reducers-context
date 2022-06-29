c40c1a6bebbf6b134d1abbbee17a75dbffd71549
sup3rcow <bullandrooster@gmail.com>
2022-06-29 13:43:14 +0200

    context with callback fn

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -2,6 +2,10 @@ import React from "react";
 
 const AuthContext = React.createContext({
   isLoggedIn: false,
+  onLogout: () => {
+    throw new Error("not implemented");
+    return;
+  },
 });
 
 export default AuthContext;
